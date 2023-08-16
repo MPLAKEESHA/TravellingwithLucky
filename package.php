ab7719dfd9bc40d0c71d4326b3f43c0a101f7226
MPLAKEESHA <127589235+MPLAKEESHA@users.noreply.github.com>
2023-08-16 20:26:25 +0530

    commiting all files to the repository

---
+++ package.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel with Lucky-package</title>
+    <!--swiper css link-->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+    <!--font awesome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!--custom css file link-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<!--header section starts-->
+
+<section class="header">
+
+    <a href="home.php" class="logo">travel</a>
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!--header section ends-->
+
+<div class="heading" style="background:url(images/header-bg-2.jpg) no-repeat">
+    <h1>packages</h1>
+</div>
+
+<!--package section starts-->
+
+<section class="packages">
+
+    <h1 class="heading-title"> top destinations</h1>
+
+    <div class="box-container">
+
+    <div class="box">
+            <div class="image">
+                <img src="images/img-1.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Enchanted Escapes Package</h3>
+                <p>Explore Europe's captivating cities, stroll cobblestone streets, and uncover the charm of fairy-tale castles on our Enchanted Escapes package </p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/img-2.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Tropical Paradise Retreat</h3>
+                <p>Immerse yourself in sun-kissed beaches, crystal-clear waters, and lush landscapes with our Tropical Paradise Retreat.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/img-3.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Cultural Odyssey Expedition</h3>
+                <p>Savor authentic cuisine, engage in immersive cultural experiences, and deepen your understanding of global heritage on our Cultural Odyssey Expedition </p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/img-4.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Historic Gems Revealed</h3>
+                <p>Uncover the secrets of the past as you walk through centuries-old streets, explore ancient ruins, and immerse yourself in the rich history that shaped civilization</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/img-5.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Wildlife Odyssey Expedition</h3>
+                <p>Immerse yourself in the animal kingdom, from breathtaking safaris in diverse habitats to encounters with majestic creatures that call the wild their home</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/img-6.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Coastal Culinary Delights</h3>
+                <p>Savor the coastal flavors and culinary treasures as you embark on a gastronomic journey, discovering seafood feasts, traditional recipes, and hidden gourmet gems</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/img-7.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Urban Safari Escape</h3>
+                <p>Embark on an urban safari adventure, exploring bustling cities, vibrant neighborhoods, and local cultures that thrive amidst the concrete jungle</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/img-8.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Serene Alpine Bliss</h3>
+                <p>Indulge in the tranquility of the Alps, where majestic peaks, alpine meadows, and serene lakes invite you to rejuvenate both body and soul</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/img-9.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Island Paradise Getaway</h3>
+                <p>Escape to secluded island havens, where palm-fringed beaches, turquoise waters, and tropical adventures await your arrival.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/img-10.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Cosmic Stargazer Expeditionr</h3>
+                <p>Venture into the night skies, unlocking the mysteries of the cosmos through astronomy tours, stargazing, and celestial wonders</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/img-11.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Rugged Outback Adventure</h3>
+                <p>Conquer the rugged landscapes of the Outback, from red deserts and iconic rock formations to indigenous culture and breathtaking sunsets</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/img-12.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Enchanting Castle Retreat</h3>
+                <p>Live like royalty as you retreat to historic castles, immerse yourself in regal grandeur, and uncover the tales of kings and queens.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+    </div>
+    <div class="load-more"><span class="btn">load more</span></div>
+</section>
+
+
+
+
+
+
+
+<!--package section ends-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--footer section starts-->
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+            <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+            <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"><i class="fas fa-phone"></i> +123-456-7890</a>
+            <a href="#"><i class="fas fa-phone"></i>+111-222-3333,</a>
+            <a href="#"><i class="fas fa-envelope"></i>shaikhanas@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i>mumbai,india-400104</a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+        </div>
+    
+    </div>
+    <div class="credit">created by <span>ms.Lucky</span> | all rights reserved! </div>
+
+</section>
+
+<!--footer section ends-->
+    
+<!--swiper js link-->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!--custom js file link-->
+<script src="js/script.js"></script>
+
+</body>
+</html>
